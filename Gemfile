bfddd1aa6077ab1a528de086490c1fddba17509e
dchandekstark <dchandekstark@gmail.com>
2014-12-04 22:54:57 -0500

    Added coveralls support

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
 
-# Specify your gem's dependencies in ezid-client.gemspec
 gemspec
+
+gem 'coveralls', require: false
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,9 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
+require 'coveralls'
+Coveralls.wear!
+
 require "ezid-client"
 
 Ezid::Client.configure do |config|
